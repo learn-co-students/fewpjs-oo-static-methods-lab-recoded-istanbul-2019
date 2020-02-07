bcfed550b09c556983c7541ab3c72b2311a4981d
Kaan <kkureli@gmail.com>
2020-02-07 18:45:50 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+    //add static methods here
+    static capitalize(string) {
+        return (string[0]).toUpperCase() + string.slice(1)
+    }
+
+    static sanitize(string) {
+        return string.replace(/[^-,'A-Za-z0-9,' ']+/g, '')
+    }
+
+
+
+    static titleize(str) {
+        let parseString = str.split(' ');
+        let ignoreThese = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+
+        parseString = parseString.map(function(elt, index) {
+            if (index == 0) return elt[0].toUpperCase() + elt.slice(1);
+            else if (ignoreThese.includes(elt)) return elt;
+            else return elt[0].toUpperCase() + elt.slice(1);
+        }).join(' ')
+        return parseString;
+    }
 }
