7d14f3e879df34e9ee01218f9e1b59d802fd23d5
Hakan Yildiz <hakan@re-coded.com>
2020-04-13 19:45:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ class Formatter {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
   static sanitize(str2){
-    return str2.replace(/[A-Za-z0-9], '');
+    return str2.replace( /[A-Za-z0-9 '-]/gi, "");
   }
   static titleize(){
     return 
