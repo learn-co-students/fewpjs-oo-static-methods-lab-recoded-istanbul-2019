482ac7a21115d1985863463d8100af379c16e58e
Hakan Yildiz <hakan@re-coded.com>
2020-04-13 20:38:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,9 @@ class Formatter {
   static titleize(str){
     let nonCap = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
     let arr = str.split(" ");
-    let empty = [];
-    arr[0]=arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
+
+    // arr[0]=arr[0].charAt(0).toUpperCase() + arr[0].slice(1);
+    arr[0] = capitalize(arr[0]);
     
     for(let i = 1; i<arr.length; i++){
         if(!nonCap.find(el => el == arr[i])){
