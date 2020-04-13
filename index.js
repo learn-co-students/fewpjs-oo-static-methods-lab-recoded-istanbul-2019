c6a672a170da71615f451f6715c22e2c0914878f
Hakan Yildiz <hakan@re-coded.com>
2020-04-13 19:44:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 class Formatter {
   //add static methods here
+  static capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  static sanitize(str2){
+    return str2.replace(/[A-Za-z0-9], '');
+  }
+  static titleize(){
+    return 
+  }
 }
