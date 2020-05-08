8aeb3a272df0d7cda6f630fcaaaab58b0f5d374b
Halit Batur <halitfuatbatur@gmail.com>
2020-05-08 23:52:50 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 class Formatter {
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-'\s]+/g,'');
+  }
+
+  static titleize(string){
+   const strArr = string.split(' ');
+   const regex = /\b(an?t?d?)\b|\b(but)\b|\b(from)\b|\b(for)\b|\b(of)\b|\b(by)\b|\b(the)\b/i;
+   strArr[0] = this.capitalize(strArr[0]);
+   return strArr.map((word, index)=>{
+   if (regex.test(word) && index !== 0){
+     return word;
+   }else{
+     return this.capitalize(word);
+   }
+   }).join(' ')
+  }
   //add static methods here
 }
