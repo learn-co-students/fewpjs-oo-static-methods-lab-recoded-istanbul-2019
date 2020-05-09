8645342bdd70dee59485466c0b54bb364299d8da
aydin <aydindavut14@gmail.com>
2020-05-10 01:30:08 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(str) {
+    let array = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = [];
+    let splited = str.split(" ")
+    for (let n = 0; n < splited.length; n++) {
+      if (n == 0) {
+        result.push(this.capitalize(splited[n]))
+      } else {
+        if (array.includes(splited[n])) {
+          result.push(splited[n])
+        } else {
+          result.push(this.capitalize(splited[n]))
+        }
+      }
+
+    }
+    return result.join(" ");
+  }
 }
