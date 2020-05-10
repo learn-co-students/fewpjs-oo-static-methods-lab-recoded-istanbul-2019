b4e0ea244618a02814df1dcc25fe708908fcd8cd
basil-kawak <basilalisa0@gmail.com>
2020-05-10 17:33:54 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9 '-]/g, '');
+  }
+  static titleize(str) {
+    let splitedStr = str.split(' ');
+    let newArr = [];
+    let regEx = /\b(a)\b|\b(and)\b|\b(of)\b|\b(at)\b|\b(but)\b|\b(from)\b|\b(the)\b|\b(in)\b|\b(an)\b|\b(for)\b|\b(by)\b/i;
+    newArr.push(this.capitalize(splitedStr[0]))
+    for (let i=1; i < splitedStr.length; i++) {
+     if (!regEx.test(splitedStr[i])) {
+        newArr.push(this.capitalize(splitedStr[i]));
+      }
+      else {
+        newArr.push(splitedStr[i]);
+      }
+  
+    }
+    return newArr.join(' ');
+  }
 }
