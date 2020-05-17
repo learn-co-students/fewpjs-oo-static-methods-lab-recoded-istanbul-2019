1278e7e51d79be863fc65da5b9c106339a32f60d
eliftabak <elifchorghay@gmail.com>
2020-05-17 04:45:58 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+      return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+  static titleize(string) {
+   let exceptions=['a','an','but','of','and','for','at','by','from','always','the']
+   let splitString = string.split(" ")
+   let titleized=[]; 
+   for(let i = 0 ; i < splitString.length ; i++) { 
+      if(i == 0){
+        titleized.push(this.capitalize(splitString[i]))
+       } else if (exceptions.includes(splitString[i])){
+      titleized.push(splitString[i]);
+       } else{
+       titleized.push(this.capitalize(splitString[i]));
+      }
+  } return titleized.join(" ");
+  }
 }
