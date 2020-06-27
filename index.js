f4a005c4a59eb27f91dd4d6f6c40125a6ef03781
refica <refia.candan@gmail.com>
2020-06-27 22:21:42 +0300

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
-}
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-'\s]/g, "");
+    //removes all non-alphanumeric characters except for dashes, single quotes and spaces.
+  };
+
+  static titleize(string){
+    let exceptions = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"];
+    let words = string.toLowerCase().split(" ");
+
+    let newArray = words.map((element, index) => {
+      if (!(exceptions.includes(element)) || index === 0)  {
+        return element.charAt(0).toUpperCase() + element.slice(1);
+      } else {
+        return element;
+      }
+    })
+    return newArray.join(" ")
+  };
+};
